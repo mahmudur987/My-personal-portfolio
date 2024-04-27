b4924b690ad0a8203b30e6a7014f065ffbe7b0cd
mahmudur987 <safemahmud987@agmail.com>
2024-04-27 12:48:08 +0600

    Update worksData.js

--- src/constant/worksData.js
+++ src/constant/worksData.js
@@ -1,105 +1,105 @@
-export const worksData = [
-  {
-    index: 1,
-    projectName: "SOCIALMATE",
-    about:
-      "Experience a vibrant social media platform designed to share thoughts and ideas seamlessly. Connect with others, express yourself through posts, comments, and likes. Enjoy features like user authentication powered by MongoDB for secure interactions.",
-    picture: "https://i.ibb.co/HxNqtZ2/Scresocialmate.png",
-    features: [
-      "Engage in social networking activities",
-      "Create and share posts",
-      "Comment on posts and interact with others",
-      "Like and unlike posts",
-      "Secure user authentication and password recovery",
-    ],
-    technologies: [
-      "React JS",
-      "Tailwind CSS",
-      "daisy-UI",
-      "Json Web Token",
-      "Node JS",
-      "Express JS",
-      "React-hot-toast",
-    ],
-    Specialty: "Utilized MongoDB for the first time for user authentication.",
-    liveSite: "https://social-mate.netlify.app/",
-    clientSite: "https://github.com/mahmudur987/socialmate",
-    serverSite: "https://github.com/mahmudur987/socialmate-server",
-  },
-  {
-    index: 2,
-    projectName: "Movie Hub",
-    about:
-      "Discover the ultimate movie-show booking platform, where users can explore movies, book tickets, and enjoy an immersive cinema experience. Seamless user login and signup along with third-party movie API integration enhance the movie-watching journey.",
-    picture: "https://i.ibb.co/xjJvw7Z/movie-hub.png",
-    features: [
-      "Browse and book movie tickets",
-      "User-friendly login and signup process",
-      "Explore detailed movie information",
-    ],
-    technologies: [
-      "React JS",
-      "Tailwind CSS",
-      "daisy-UI",
-      "Node JS",
-      "Express JS",
-      "React-hot-toast",
-    ],
-    Specialty:
-      "Incorporated a third-party movie API for enhanced functionality.",
-    liveSite: "https://movie-hub9.netlify.app/",
-    clientSite: "https://github.com/mahmudur987/movie-hub",
-    serverSite: "https://github.com/mahmudur987/movie-hub",
-  },
-  {
-    index: 3,
-    projectName: "D-Routin",
-    about:
-      "Manage your daily routine effortlessly with D-Routin. Keep track of tasks, set reminders, and streamline your daily activities. With user authentication and an intuitive interface, organizing your routine has never been easier.",
-    picture: "https://i.ibb.co/HTXrKJp/daily-Routin.png",
-    features: [
-      "Create and manage daily tasks",
-      "User-friendly task addition and deletion",
-      "Efficient user authentication process",
-    ],
-    technologies: [
-      "React JS",
-      "Tailwind CSS",
-      "daisy-UI",
-      "Node JS",
-      "Express JS",
-    ],
-    Specialty:
-      "A standalone web application designed for seamless task management.",
-    liveSite: "https://d-routin.netlify.app/",
-    clientSite: "https://github.com/mahmudur987/D-routin",
-    serverSite: "https://github.com/mahmudur987/D-Routin-server",
-  },
-  {
-    index: 4,
-    projectName: "Magazine",
-    about:
-      "Immerse yourself in a captivating reading experience with Magazine, a platform dedicated to storytelling and blog sharing. Discover engaging stories, user-friendly navigation, and seamless admin functionalities.",
-    picture: "https://i.ibb.co/G5h5rcn/magazine.png",
-    features: [
-      "Explore and read captivating stories",
-      "User-friendly login and signup processes",
-      "Admin dashboard for content management",
-      "Create and delete blog posts",
-    ],
-    technologies: [
-      "React JS",
-      "Tailwind CSS",
-      "daisy-UI",
-      "Redux",
-      "React-hot-toast",
-    ],
-    Specialty: "Utilized Redux for state management without a backend.",
-    liveSite: "https://magazine987.netlify.app/",
-    clientSite: "https://github.com/mahmudur987/magazine",
-    serverSite: "https://github.com/mahmudur987/magazine",
-  },
-];
+// export const worksData = [
+//   {
+//     index: 1,
+//     projectName: "SOCIALMATE",
+//     about:
+//       "Experience a vibrant social media platform designed to share thoughts and ideas seamlessly. Connect with others, express yourself through posts, comments, and likes. Enjoy features like user authentication powered by MongoDB for secure interactions.",
+//     picture: "https://i.ibb.co/HxNqtZ2/Scresocialmate.png",
+//     features: [
+//       "Engage in social networking activities",
+//       "Create and share posts",
+//       "Comment on posts and interact with others",
+//       "Like and unlike posts",
+//       "Secure user authentication and password recovery",
+//     ],
+//     technologies: [
+//       "React JS",
+//       "Tailwind CSS",
+//       "daisy-UI",
+//       "Json Web Token",
+//       "Node JS",
+//       "Express JS",
+//       "React-hot-toast",
+//     ],
+//     Specialty: "Utilized MongoDB for the first time for user authentication.",
+//     liveSite: "https://social-mate.netlify.app/",
+//     clientSite: "https://github.com/mahmudur987/socialmate",
+//     serverSite: "https://github.com/mahmudur987/socialmate-server",
+//   },
+//   {
+//     index: 2,
+//     projectName: "Movie Hub",
+//     about:
+//       "Discover the ultimate movie-show booking platform, where users can explore movies, book tickets, and enjoy an immersive cinema experience. Seamless user login and signup along with third-party movie API integration enhance the movie-watching journey.",
+//     picture: "https://i.ibb.co/xjJvw7Z/movie-hub.png",
+//     features: [
+//       "Browse and book movie tickets",
+//       "User-friendly login and signup process",
+//       "Explore detailed movie information",
+//     ],
+//     technologies: [
+//       "React JS",
+//       "Tailwind CSS",
+//       "daisy-UI",
+//       "Node JS",
+//       "Express JS",
+//       "React-hot-toast",
+//     ],
+//     Specialty:
+//       "Incorporated a third-party movie API for enhanced functionality.",
+//     liveSite: "https://movie-hub9.netlify.app/",
+//     clientSite: "https://github.com/mahmudur987/movie-hub",
+//     serverSite: "https://github.com/mahmudur987/movie-hub",
+//   },
+//   {
+//     index: 3,
+//     projectName: "D-Routin",
+//     about:
+//       "Manage your daily routine effortlessly with D-Routin. Keep track of tasks, set reminders, and streamline your daily activities. With user authentication and an intuitive interface, organizing your routine has never been easier.",
+//     picture: "https://i.ibb.co/HTXrKJp/daily-Routin.png",
+//     features: [
+//       "Create and manage daily tasks",
+//       "User-friendly task addition and deletion",
+//       "Efficient user authentication process",
+//     ],
+//     technologies: [
+//       "React JS",
+//       "Tailwind CSS",
+//       "daisy-UI",
+//       "Node JS",
+//       "Express JS",
+//     ],
+//     Specialty:
+//       "A standalone web application designed for seamless task management.",
+//     liveSite: "https://d-routin.netlify.app/",
+//     clientSite: "https://github.com/mahmudur987/D-routin",
+//     serverSite: "https://github.com/mahmudur987/D-Routin-server",
+//   },
+//   {
+//     index: 4,
+//     projectName: "Magazine",
+//     about:
+//       "Immerse yourself in a captivating reading experience with Magazine, a platform dedicated to storytelling and blog sharing. Discover engaging stories, user-friendly navigation, and seamless admin functionalities.",
+//     picture: "https://i.ibb.co/G5h5rcn/magazine.png",
+//     features: [
+//       "Explore and read captivating stories",
+//       "User-friendly login and signup processes",
+//       "Admin dashboard for content management",
+//       "Create and delete blog posts",
+//     ],
+//     technologies: [
+//       "React JS",
+//       "Tailwind CSS",
+//       "daisy-UI",
+//       "Redux",
+//       "React-hot-toast",
+//     ],
+//     Specialty: "Utilized Redux for state management without a backend.",
+//     liveSite: "https://magazine987.netlify.app/",
+//     clientSite: "https://github.com/mahmudur987/magazine",
+//     serverSite: "https://github.com/mahmudur987/magazine",
+//   },
+// ];
 
 export const RunningProjects = [
   {
